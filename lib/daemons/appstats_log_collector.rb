4effe98e363c327ba2266fbc735267c6ff912acb
Andrew Forward <aforward@gmail.com>
2011-02-09 16:39:59 -0500

    Re-establish connection because we are sleeping so long

--- lib/daemons/appstats_log_collector.rb
+++ lib/daemons/appstats_log_collector.rb
@@ -32,11 +32,13 @@ require File.join(File.dirname(__FILE__),"..","appstats")
 
 Appstats.log(:info,"Started Appstats Log Collector")
 while($running) do
+  ActiveRecord::Base.connection.reconnect!
   appstats_config["remote_servers"].each do |remote_server|
     Appstats::LogCollector.find_remote_files(remote_server,remote_server[:path],remote_server[:template])
   end
   Appstats::LogCollector.download_remote_files(appstats_config["remote_servers"])
   Appstats::LogCollector.process_local_files
+  ActiveRecord::Base.connection.disconnect!
   a_day_in_seconds = 60*60*24
   sleep a_day_in_seconds
 end
